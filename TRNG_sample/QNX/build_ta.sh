1a0aa10c9e3f37d65f44fd40dd02bd5e8067ad1f
Nhi Nguyen <nhi.nguyen.kx@renesas.com>
2025-05-28 15:02:10 +0700

    TRNG QNX build: Update the build script about qnx710 toolchain
    Signed-off-by: Nhi Nguyen <nhi.nguyen.kx@renesas.com>

--- TRNG_sample/QNX/build_ta.sh
+++ TRNG_sample/QNX/build_ta.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-	echo "Error: You need to enter 2 parameters when running the script."
-	echo "Usage: ./build.sh <OPTEE_OS>"
-	exit 1
-fi
-
 unset LD_LIBRARY_PATH
 
 ## Prepare environment variable to build TA
-export OPTEE_OS=$1
+export OPTEE_OS=/data/nhinguyen/_optee/gen4/_for_V4H/v430/optee_os		# user need to change this path
 export TA_DEV_KIT_DIR="${OPTEE_OS}/out/arm-plat-rcar_gen4/export-ta_arm64"
-export SDK=/data/tutran/sdk		# Users need to adjust to SDK version 3.1.11
+export SDK=/data/nhinguyen/_optee/sdk_adas		# Users need to adjust to SDK version 3.1.11
 export OPTEE_CLIENT_EXPORT=${SDK}/sysroots/aarch64-poky-linux/usr
 . ${SDK}/environment-setup-aarch64-poky-linux
 export PATH=/shsv/Android/SoftIP/10_DienPham/GitlabCI/software/python_dir/bin:${PATH} #User need to adjust the python version
